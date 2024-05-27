9fee50a63799f867853b7475cbbb976a06dfc73a
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2024-05-27 16:44:33 -0400

    Android Adjustment

--- GetUserInput.js
+++ GetUserInput.js
@@ -1,10 +1,15 @@
 // getUserInput.js
 
 const readlineSync = require('readline-sync');
+const { execSync } = require('child_process');
 
 // Function to get user input synchronously
 function getUserInput(prompt) {
     return readlineSync.question(prompt).trim();
+}
+
+function getUserInput(prompt) {
+    return execSync('termux-dialog text input -t "User Input" -i ""').toString().trim();
 }
 
 module.exports = getUserInput;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GREVocab
+# Code contains weights of vocab words. Correct answers are lower in weight while wrong are higher.
+# Higher weights are more likely to be selected.
